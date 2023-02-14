076c5ab396dac386ffcbdcf3f71419be8119ff50
vityha20022 <vityha20022@mail.ru>
2023-02-15 02:24:38 +0300

    Add tab bar as root view controller

---
+++ MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  PhotosApp
+//
+//  Created by Виктор Борисовский on 15.02.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Виктор Борисовский on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
