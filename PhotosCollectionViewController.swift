eaa35738f55bff24e9abbb692e4170f8f0cbf685
vityha20022 <vityha20022@mail.ru>
2023-02-15 03:20:25 +0300

    Setup PhotosCollectionViewController

--- PhotosCollectionViewController.swift
+++ PhotosCollectionViewController.swift
@@ -12,5 +12,54 @@ class PhotosCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .orange
+        setupNavigationBar()
+        setupCollectionView()
+        setupSearchBar()
+    }
+    
+    // MARK: - Setup UI Elements
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = .gray
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+    }
+    
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+    
+    // MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension PhotosCollectionViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
