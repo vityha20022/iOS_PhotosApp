78b916b3fe3657a3e2f13c02aab091e32dbfae39
vityha20022 <vityha20022@mail.ru>
2023-02-15 22:36:46 +0300

    Setup info labels

--- PhotoDetailInformationViewController.swift
+++ PhotoDetailInformationViewController.swift
@@ -29,20 +29,59 @@ class PhotoDetailInformationViewController: UIViewController {
         }
     }
     
+    var previous: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         
         view.addSubview(photoImageView)
-        photoImageView.image = photoImageView.image?.resized(to: CGSize(width: 400, height: 300))
+        photoImageView.image = photoImageView.image?.resized(to: CGSize(width: 400, height: 250))
         
         photoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         photoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
         
         photoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
         
+        setupLabels()
+    }
+    
+    private func addInfoLabels(headerName: String, info: String) {
+        let headerLabel = UILabel()
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        headerLabel.text = headerName
         
+        view.addSubview(headerLabel)
+        headerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        headerLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10).isActive = true
+        headerLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        if let previous = previous {
+            headerLabel.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+        } else {
+            headerLabel.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: 10).isActive = true
+        }
+        
+        let infoLabel = UILabel()
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        infoLabel.font = UIFont.systemFont(ofSize: 15)
+        infoLabel.text = info
+        
+        view.addSubview(infoLabel)
+        infoLabel.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 10).isActive = true
+        infoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        infoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10).isActive = true
+        infoLabel.heightAnchor.constraint(equalToConstant: 15).isActive = true
+        
+        previous = infoLabel
+    }
+    
+    private func setupLabels() {
+        addInfoLabels(headerName: "Author", info: "Victor Borisovskiy")
+        addInfoLabels(headerName: "Created at", info: "05.12.1998")
+        addInfoLabels(headerName: "Location", info: "Omsk")
+        addInfoLabels(headerName: "Downloads", info: "235")
     }
 }
 
